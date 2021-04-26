dabfe193c3efd2420f0a734abdc749f6cd3d7b34
home_machine <illust.training@gmail.com>
2021-04-26 12:48:56 +0900

    create DNU model

---
+++ model/DNU.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+
+import torch
+
+
+class DNU_Block(torch.nn.Module):
+
+    def __init__(self, input_ch: int, output_ch: int, *args, feature_num: int=64, **kwargs):
+        super(DNU_Block, self).__init__()
+
+        pass
+
+
+class DNU(torch.nn.Module):
+
+    def __init__(self, input_ch: int, output_ch: int, *args, feature_num: int=64, **kwargs):
+        super(DNU, self).__init__()
+
+        pass
