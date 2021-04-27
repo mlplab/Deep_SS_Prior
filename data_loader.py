569b6e12122d361a3f6d53fb1a822f255e3d787f
home_machine <illust.training@gmail.com>
2021-04-27 09:37:05 +0900

    create dnu dataloader

--- data_loader.py
+++ data_loader.py
@@ -14,42 +14,6 @@ data_path = 'dataset/'
 size = 256
 
 
-class HyperSpectralDataset(torch.utils.data.Dataset):
-
-    def __init__(self, img_path, mask_path, concat=False, tanh=False, data_key='data', transform=None):
-
-        self.img_path = img_path
-        self.data = os.listdir(img_path)
-        # self.mask = sio.loadmat(os.path.join(img_path, mask_path))
-        mask = sio.loadmat(mask_path)['data'].astype(np.float32)
-        self.mask = torchvision.transforms.ToTensor()(mask)
-        self.data_len = len(self.data)
-        self.tanh = tanh
-        self.concat = concat
-        self.data_key = data_key
-        self.transforms = transform
-
-    def __getitem__(self, idx):
-        mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
-        nd_data = np.array(mat_data, dtype=np.float32).copy()
-        if self.transforms is not None:
-            for transform in self.transforms:
-                nd_data = transform(nd_data)
-        else:
-            nd_data = torchvision.transforms.ToTensor()(nd_data)
-        trans_data = nd_data
-        label_data = trans_data
-        measurement_data = (trans_data * self.mask).sum(dim=0, keepdim=True)
-        if self.concat is True:
-            input_data = torch.cat([measurement_data, self.mask], dim=0)
-        else:
-            input_data = measurement_data
-        return input_data, label_data
-
-    def __len__(self):
-        return self.data_len
-
-
 class PatchMaskDataset(torch.utils.data.Dataset):
 
     def __init__(self, img_path, mask_path, concat=False, tanh=False, data_key='data', transform=None):
@@ -155,4 +119,30 @@ class PatchMasDataset_ISTA(PatchMaskDataset):
             input_data = torch.cat([measurement_data, mask], dim=0)
         else:
             input_data = measurement_data
+        return input_data, label_data
+
+
+class DNUDataset(PatchMaskDataset):
+
+    def __getitem__(self, idx):
+        patch_id = self.data[idx].split('.')[0].split('_')[-1]
+        mat_data = sio.loadmat(os.path.join(self.img_path, self.data[idx]))[self.data_key]
+        nd_data = np.array(mat_data, dtype=np.float32).copy()
+        if self.transforms is not None:
+            for transform in self.transforms:
+                nd_data = transform(nd_data)
+        else:
+            nd_data = torchvision.transforms.ToTensor()(nd_data)
+        trans_data = nd_data
+        label_data = trans_data
+        mask = sio.loadmat(os.path.join(self.mask_path, f'mask_{patch_id}.mat'))[self.data_key]
+        mask = self.mask_transforms(mask)
+        measurement_data = (trans_data * mask).sum(dim=0, keepdim=True)
+        measurement_data = measurement_data.tile(1, label_data.shape[1], 1, 1)
+        measurement_data = measurement_data * mask
+        if self.concat is True:
+            measurement_data = torch.cat([measurement_data, mask], dim=0)
+        else:
+            measurement_data = measurement_data
+        input_data = [measurement_data, mask]
         return input_data, label_data
