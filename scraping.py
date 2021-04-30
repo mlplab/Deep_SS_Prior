7b05ca8df0792523d2c590645045a143a9541ea0
lab_manjaro <labpc2019@gmail.com>
2021-04-30 16:26:28 +0900

    Downlaod ICVL

--- scraping.py
+++ scraping.py
@@ -3,12 +3,16 @@
 
 import os
 import re
+import time
+import urllib
 import requests
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
 http_proxy = os.environ.get('HTTP_PROXY')
 https_proxy = os.environ.get('HTTPs_PROXY')
+
 
 proxies = {
         "http": http_proxy,
@@ -16,17 +20,24 @@ proxies = {
 }
 
 
-url = 'http://icvl.cs.bgu.ac.il/img/hs_pub/'
+url = 'http://icvl.cs.bgu.ac.il/img/hs_pub'
+save_dir = '../SCI_dataset/Download_ICVL'
+os.makedirs(save_dir, exist_ok=True)
 
 
 if __name__ == '__main__':
-    response = requests.get(url, proxies=proxies)
-    with open('icvl_list.html', 'w') as f:
-        print(response.text, file=f)
-    response.encoding = response.apparent_encoding
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # response = requests.get(url, proxies=proxies)
+    # with open('icvl_list.html', 'w') as f:
+    #     print(response.text, file=f)
+    # response.encoding = response.apparent_encoding
+    with open('icvl_list.html', 'r') as f:
+        html_text = f.read()
+    soup = BeautifulSoup(html_text, 'html.parser')
     soup_a = soup.find_all('a')
-    p = re.compile(r"<[^>]*?>")
-    for tag_str in soup_a:
-        p.sub("", tag_str)
-        print(tag_str)
+    img_list = [tag_str.text for tag_str in soup_a if tag_str.text.split('.')[-1] == 'mat']
+    for name in tqdm(img_list):
+        with urllib.request.urlopen(os.path.join(url, name)) as web_file:
+            data = web_file.read()
+            with open(os.path.join(save_dir, name), 'wb') as f:
+                f.write(data)
+        time.sleep(1)
