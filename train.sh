8f98d2fa4288361368441116dbc4b131d03c97c4
mlplab <labpc2019@gmail.com>
2021-05-17 16:29:41 +0900

    unk2

--- train.sh
+++ train.sh
@@ -30,6 +30,7 @@ do
 done
 
 
+
 echo $batch_size
 echo $epoch
 echo $dataset
@@ -40,6 +41,21 @@ echo $start_time
 model_name=( `echo $model_name | tr ' ' ' '` )
 datasets=( `echo $datasets | tr ' ' ' '` )
 for dataset in $datasets[@]; do
+    if [ ! -e "../SCI_ckpt/${dataset}_SOTA" ]; then
+        echo "To make trained SOTA model."
+        exit 0
+    fi
+    ckpt_path="../SCI_ckpt/${dataset}_${start_time}"
+    all_trained_path="${ckpt_path}/all_trained"
+    mkdir -p $all_trained_path
+    echo $all_trained_path
+    for file in $(ls "../SCI_ckpt/${dataset}_SOTA"); do
+        cp "../SCI_ckpt/${dataset}_SOTA/${file}" "${all_trained_path}/${file}"
+    done
+done
+
+
+for dataset in $datasets[@]; do
     for name in $model_name[@]; do
         echo $model_name
         if [ $name = "FusionReconst" ]; then
@@ -47,7 +63,9 @@ for dataset in $datasets[@]; do
                 python train_sh.py -b $batch_size -e $epoch -d $dataset -c $concat -m $name -bn $block_num -st $start_time -fb $feature_block
             done
         else
-            python train_sh.py -b $batch_size -e $epoch -d $dataset -c $concat -m $name -bn $block_num -st $start_time
+            if [ ! -e "${all_trained_path}/${model_name}_09.tar"]; then
+                python train_sh.py -b $batch_size -e $epoch -d $dataset -c $concat -m $name -bn $block_num -st $start_time
+            fi
         fi
     done
 done
